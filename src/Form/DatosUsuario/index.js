f53293c2b6f305de3ad04ffcb8f62951b6b0ee83
Atilio <marianoatilio@gmail.com>
2023-09-28 20:04:50 -0300

    Modulo 1

---
+++ src/Form/DatosUsuario/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+import { valdidarEmail, validarPassword } from "./validaciones";
+
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({
+    value: "",
+    valid: true,
+  });
+  const [password, setPassword] = useState({ value: "", valid: true });
+
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (email.valid && password.valid) {
+          console.log("Siguiente formulario");
+        } else {
+          console.log("No hacer nada");
+        }
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={email.valid === false}
+        helperText={
+          email.valid === false && "Ingresa un correo electrónico válido"
+        }
+        value={email.value}
+        onChange={(input) => {
+          const valorEmail = input.target.value;
+          const valido = valdidarEmail(valorEmail);
+          setEmail({ value: valorEmail, valid: valido });
+        }}
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        error={password.valid === false}
+        helperText={
+          password.valid === false &&
+          "Ingrese un contraseña valida, minimo 8 caracteres y maximo 20."
+        }
+        value={password.value}
+        onChange={(input) => {
+          const valorPassword = input.target.value;
+          setPassword({
+            value: valorPassword,
+            valid: validarPassword(valorPassword),
+          });
+        }}
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
+};
+
+export default DatosUsuario;
